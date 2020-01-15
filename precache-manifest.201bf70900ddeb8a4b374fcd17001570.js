c79fad4ba8765b650bee573bba782aa002aff01f
Fatih Kaya <fatih.55kaya.1@gmail.com>
2020-01-15 18:38:22 +0300

    Updates

---
+++ precache-manifest.201bf70900ddeb8a4b374fcd17001570.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4554ec5d3c2e84a2d82d8f16b23b5d",
+    "url": "/zindex/index.html"
+  },
+  {
+    "revision": "7a691c652cdba2a6d287",
+    "url": "/zindex/static/js/2.5d6cbabd.chunk.js"
+  },
+  {
+    "revision": "e8e725cc0318913c61fa",
+    "url": "/zindex/static/js/main.eda445d1.chunk.js"
+  },
+  {
+    "revision": "7eb61936f550f9214d5b",
+    "url": "/zindex/static/js/runtime-main.86a0a8b8.js"
+  },
+  {
+    "revision": "a93d8e1a89f9100d7e04c98ebcbc0933",
+    "url": "/zindex/static/media/bg.a93d8e1a.mp3"
+  },
+  {
+    "revision": "8d516cce9b80ee257214da40ace5c474",
+    "url": "/zindex/static/media/click.8d516cce.mp3"
+  },
+  {
+    "revision": "12d1603454265a90269ca13e0d6c04b2",
+    "url": "/zindex/static/media/earth.12d16034.jpg"
+  },
+  {
+    "revision": "e896d081c42d144db443a0faeea36cf8",
+    "url": "/zindex/static/media/engine.e896d081.mp3"
+  },
+  {
+    "revision": "bdb080913490e33da93bb6510d530d4d",
+    "url": "/zindex/static/media/engine2.bdb08091.mp3"
+  },
+  {
+    "revision": "5c5ce6974ee4192d9ecbc74242daa4ce",
+    "url": "/zindex/static/media/explosion.5c5ce697.mp3"
+  },
+  {
+    "revision": "a97c8e670e85efbd49691d7e343da830",
+    "url": "/zindex/static/media/laser.a97c8e67.mp3"
+  },
+  {
+    "revision": "85e26bcf5b6a8267ea0e883a15d40531",
+    "url": "/zindex/static/media/moon.85e26bcf.png"
+  },
+  {
+    "revision": "5699f1c4aff5845551ef0ba1a7ad31f8",
+    "url": "/zindex/static/media/warp.5699f1c4.mp3"
+  }
+]);
